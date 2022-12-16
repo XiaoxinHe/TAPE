331e61664be928847af439b27de661eb8f182205
Cautiousss <862405237@qq.com>
2022-12-16 16:04:37 +0800

    dataset

--- main_pubmed_gnn.py
+++ main_pubmed_gnn.py
@@ -16,11 +16,11 @@ class GCN(torch.nn.Module):
         self.convs.append(GCNConv(in_channels, hidden_channels, cached=True))
         self.bns = torch.nn.ModuleList()
         self.bns.append(torch.nn.BatchNorm1d(hidden_channels))
-        for _ in range(num_layers - 2):
+        for _ in range(num_layers - 1):
             self.convs.append(
                 GCNConv(hidden_channels, hidden_channels, cached=True))
             self.bns.append(torch.nn.BatchNorm1d(hidden_channels))
-        self.convs.append(GCNConv(hidden_channels, out_channels, cached=True))
+        self.readout = torch.nn.Linear(hidden_channels, out_channels)
         self.dropout = dropout
 
     def reset_parameters(self):
@@ -29,13 +29,14 @@ class GCN(torch.nn.Module):
         for bn in self.bns:
             bn.reset_parameters()
 
-    def forward(self, x, adj_t):
+    def forward(self, x, adj_t, readout=True):
         for i, conv in enumerate(self.convs[:-1]):
             x = conv(x, adj_t)
             x = self.bns[i](x)
             x = F.relu(x)
             x = F.dropout(x, p=self.dropout, training=self.training)
-        x = self.convs[-1](x, adj_t)
+        if readout:
+            x = self.readout(x)
         return x
 
 
