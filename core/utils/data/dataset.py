42cf46fe3c715cce18bdfd624c64990bfbd61c4a
Cautiousss <862405237@qq.com>
2023-02-28 16:16:31 +0800

    feat: kd (logits)

--- core/utils/data/dataset.py
+++ core/utils/data/dataset.py
@@ -7,7 +7,7 @@ class Dataset(torch.utils.data.Dataset):
         self.encodings = encodings
         self.labels = labels
         self.features = features
-        self.gamma=gamma
+        self.gamma = gamma
 
     def __getitem__(self, idx):
         item = {key: torch.tensor(val[idx])
@@ -19,6 +19,27 @@ class Dataset(torch.utils.data.Dataset):
             item["features"] = self.features[idx]
         if self.gamma is not None:
             item["gamma"] = self.gamma[idx]
+        return item
+
+    def __len__(self):
+        return len(self.encodings["input_ids"])
+
+
+class KDDataset(torch.utils.data.Dataset):
+    def __init__(self, encodings, labels=None, pred_t=None):
+        self.encodings = encodings
+        self.labels = labels
+        self.pred_t = pred_t
+
+    def __getitem__(self, idx):
+        item = {key: torch.tensor(val[idx])
+                for key, val in self.encodings.items()}
+        item['node_id'] = idx
+        if self.labels:
+            item["labels"] = torch.tensor(self.labels[idx])
+        if self.pred_t is not None:
+            item["pred_t"] = self.pred_t[idx]
+
         return item
 
     def __len__(self):
