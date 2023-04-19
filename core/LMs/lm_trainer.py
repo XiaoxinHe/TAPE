0c621309e407ed3ca78b2d6585c36615b5f7780d
Cautiousss <862405237@qq.com>
2023-04-19 21:02:01 +0800

    feat: finetune LM

--- core/LMs/lm_trainer.py
+++ core/LMs/lm_trainer.py
@@ -1,60 +1,86 @@
 import torch
 import numpy as np
 from core.utils.data.dataset import Dataset
-from transformers import AutoTokenizer, AutoModel, TrainingArguments, Trainer, EarlyStoppingCallback
-from core.LMs.model import BertEmb
+from transformers import AutoTokenizer, AutoModel, TrainingArguments, Trainer, IntervalStrategy
+from core.LMs.model import BertClassifier, BertClaInfModel
 
 from core.LMs.lm_utils import load_data
 from core.LMs.lm_utils import compute_metrics
-from core.utils.function.os_utils import init_path
-
-feat_shrink = 128
+from core.utils.function.os_utils import init_path, time_logger
 
 
 class LMTrainer():
     def __init__(self, args):
-        self.model_name = "microsoft/deberta-base"
+        self.model_name = args.model
         self.dataset_name = args.dataset
+        self.seed = args.seed
 
-    def train(self):
+        self.feat_shrink = args.feat_shrink
+        self.weight_decay = args.weight_decay
+        self.dropout = args.dropout
+        self.att_dropout = args.att_dropout
+        self.cla_dropout = args.cla_dropout
+
+        self.batch_size = args.batch_size
+        self.epochs = args.epochs
+        self.warmup_epochs = args.warmup_epochs
+        self.eval_patience = args.eval_patience
+        self.lr = args.lr
+
+        self.ckpt = f"output/{self.dataset_name}/{self.model_name}.ckpt"
+
         # Preprocess data
         data, text = load_data(dataset=self.dataset_name, use_text=True)
         self.num_nodes = data.x.size(0)
+        self.n_labels = data.y.unique().size(0)
 
-        # Define pretrained tokenizer and model
         tokenizer = AutoTokenizer.from_pretrained(self.model_name)
         X = tokenizer(text, padding=True, truncation=True, max_length=512)
-        self.dataset = Dataset(X, data.y.tolist())
+        dataset = Dataset(X, data.y.tolist())
 
         self.train_dataset = torch.utils.data.Subset(
-            self.dataset, data.train_mask.nonzero().squeeze().tolist())
+            dataset, data.train_mask.nonzero().squeeze().tolist())
         self.val_dataset = torch.utils.data.Subset(
-            self.dataset, data.val_mask.nonzero().squeeze().tolist())
+            dataset, data.val_mask.nonzero().squeeze().tolist())
         self.test_dataset = torch.utils.data.Subset(
-            self.dataset, data.test_mask.nonzero().squeeze().tolist())
+            dataset, data.test_mask.nonzero().squeeze().tolist())
 
+        self.data = data
+        self.inf_dataset = dataset
+
+    def train(self):
+        train_steps = self.num_nodes // self.batch_size + 1
+        eval_steps = self.eval_patience // self.batch_size
+        warmup_steps = int(self.warmup_epochs * train_steps)
+
+        # Define pretrained tokenizer and model
         bert_model = AutoModel.from_pretrained(self.model_name)
-
-        self.model = BertEmb(bert_model,
-                             n_labels=data.y.unique().size(0),
-                             feat_shrink=feat_shrink)
+        self.model = BertClassifier(bert_model,
+                                    n_labels=self.n_labels,
+                                    feat_shrink=self.feat_shrink)
+        self.model.config.dropout = self.dropout
+        self.model.config.attention_dropout = self.att_dropout
 
         # Define Trainer
         args = TrainingArguments(
-            output_dir="output",
-            logging_steps=10,
-            evaluation_strategy="steps",
-            eval_steps=50,
-            per_device_train_batch_size=8,
-            per_device_eval_batch_size=16,
-            num_train_epochs=5,
-            seed=0,
+            output_dir=f'output/{self.dataset_name}/{self.model_name}',
+            do_train=True,
+            do_eval=True,
+            eval_steps=eval_steps,
+            evaluation_strategy=IntervalStrategy.STEPS,
+            save_steps=eval_steps,
+            learning_rate=self.lr,
+            weight_decay=self.weight_decay,
+            save_total_limit=1,
             load_best_model_at_end=True,
-            disable_tqdm=True,
+            gradient_accumulation_steps=1,
+            per_device_train_batch_size=self.batch_size,
+            per_device_eval_batch_size=self.batch_size*8,
+            warmup_steps=warmup_steps,
+            num_train_epochs=self.epochs,
             dataloader_num_workers=1,
+            fp16=True,
             dataloader_drop_last=True,
-            weight_decay=0.01,
-            # learning_rate=2e-5
         )
         self.trainer = Trainer(
             model=self.model,
@@ -62,36 +88,57 @@ class LMTrainer():
             train_dataset=self.train_dataset,
             eval_dataset=self.val_dataset,
             compute_metrics=compute_metrics,
-            callbacks=[EarlyStoppingCallback(early_stopping_patience=3)],
+            # callbacks=[EarlyStoppingCallback(early_stopping_patience=3)],
         )
 
         # Train pre-trained model
         self.trainer.train()
+        torch.save(self.model.state_dict(), init_path(self.ckpt))
 
+    @time_logger
     @torch.no_grad()
     def eval_and_save(self):
-        torch.save(self.model.state_dict(), init_path(
-            f"output/{self.dataset_name}/bert0.pt"))
-        ckpt_emb = np.memmap(init_path(f"output/{self.dataset_name}/bert.emb0"), dtype=np.float32, mode='w+', shape=(
-            self.num_nodes, feat_shrink if feat_shrink else 768))
-        self.model.ckpt_emb = ckpt_emb
+        emb = np.memmap(init_path(f"output/{self.dataset_name}/{self.model_name}.emb"),
+                        dtype=np.float16,
+                        mode='w+',
+                        shape=(self.num_nodes, self.feat_shrink if self.feat_shrink else 768))
+        pred = np.memmap(init_path(f"output/{self.dataset_name}/{self.model_name}.pred"),
+                         dtype=np.float16,
+                         mode='w+',
+                         shape=(self.num_nodes, self.n_labels))
 
-        # Define Trainer
-        args = TrainingArguments(
-            output_dir="output",
+        inf_model = BertClaInfModel(
+            self.model, emb, pred, feat_shrink=self.feat_shrink)
+        inf_model.eval()
+        inference_args = TrainingArguments(
+            output_dir=f'output/{self.dataset_name}/{self.model_name}',
             do_train=False,
             do_predict=True,
-            per_device_eval_batch_size=16,
+            per_device_eval_batch_size=self.batch_size*8,
             dataloader_drop_last=False,
             dataloader_num_workers=1,
-            disable_tqdm=True,
+            fp16_full_eval=True,
         )
-        trainer = Trainer(model=self.model,
-                          args=args,
-                          compute_metrics=compute_metrics)
 
-        train_metrics = trainer.predict(self.train_dataset).metrics
-        val_metrics = trainer.predict(self.val_dataset).metrics
-        test_metrics = trainer.predict(self.test_dataset).metrics
+        trainer = Trainer(model=inf_model, args=inference_args)
+        trainer.predict(self.inf_dataset)
+        if "ogbn" in self.dataset_name:
+            from ogb.nodeproppred import Evaluator
+            _evaluator = Evaluator(name=self.dataset_name)
+        else:
+            from core.GNNs.gnn_utils import Evaluator
+            _evaluator = Evaluator(name=self.dataset_name)
 
-        print(test_metrics)
+        def evaluator(preds, labels): return _evaluator.eval({
+            "y_true": torch.tensor(labels).view(-1, 1),
+            "y_pred": torch.tensor(preds).view(-1, 1),
+        })["acc"]
+
+        def eval(x): return evaluator(
+            np.argmax(pred[x], -1), self.data.y[x])
+
+        res = {
+            'lm_train_acc': eval(self.data.train_mask),
+            'lm_val_acc': eval(self.data.val_mask),
+            'lm_test_acc': eval(self.data.test_mask)}
+        print(res)
